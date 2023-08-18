478b2ac6861c5b57ecc88a29815987844ccb919f
Pechenkin.A <pechenkin685@gmail.com>
2023-08-18 12:43:05 +0700

    Первый коммит

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ['@element-plus/nuxt'],
+
+  //@ts-ignore
+  css: [
+    '@fortawesome/fontawesome-svg-core/styles.css'
+  ]
+})
