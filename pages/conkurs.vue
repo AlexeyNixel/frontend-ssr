8844257dc2ba5b11822aef40128787c61696d648
Pechenkin.A <pechenkin685@gmail.com>
2024-03-13 13:11:35 +0700

    Фикс конкурсов

--- pages/conkurs.vue
+++ pages/conkurs.vue
@@ -10,40 +10,32 @@ const { metaEntry } = storeToRefs(entryStore);
 
 const page = ref<number>(Number(route.query.page) || 1);
 
-const swapPage = async () => {
-  navigateTo({ path: '/conkurs/', query: { page: page.value } });
-  if (process.client) window.scroll(0, 0);
-  entries.value = await entryStore.getEntriesByRubric('konkursy', {
+const fetchData = async () => {
+  entries.value = await entryStore.getEntries({
+    rubric: 'konkursy',
     include: 'preview',
     orderBy: '-publishedAt',
     page: page.value,
   });
 };
 
+const swapPage = async () => {
+  navigateTo({ path: '/conkurs/', query: { page: page.value } });
+  if (process.client) window.scroll(0, 0);
+  await fetchData();
+};
+
 onMounted(async () => {
-  entries.value = await entryStore.getEntriesByRubric('konkursy', {
-    include: 'preview',
-    orderBy: '-publishedAt',
-    page: page.value,
-  });
+  await fetchData();
 });
 </script>
 
 <template>
-  <div class="flex flex-col">
+  <div class="flex flex-col" v-if="entries?.data">
     <div class="text-2xl font-bold my-2">Конкурсы</div>
-    <the-entry
-      v-for="item in entries"
-      :key="item.id"
-      :entry="item"
-      :is-date="true"
-    />
-    <UPagination
-      class="flex items-center justify-center my-4"
-      v-model="page"
-      @update:model-value="swapPage()"
-      :total="metaEntry.pages * 10"
-    />
+    <the-entry v-for="item in entries.data" :key="item.id" :entry="item" :is-date="true" />
+    <UPagination class="flex items-center justify-center my-4" v-model="page" @update:model-value="swapPage()"
+      :total="entries?.meta.total" />
   </div>
 </template>
 
