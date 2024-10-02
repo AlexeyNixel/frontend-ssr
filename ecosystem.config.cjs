92645ba569d35ae30261106bf6c494b1cb5271aa
Pechenkin.A <pechenkin685@gmail.com>
2024-10-02 11:48:18 +0700

    Добавлен деплой с использованием pm2

---
+++ ecosystem.config.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'NuxtAppName',
+      port: '3000',
+      exec_mode: 'cluster',
+      instances: 'max',
+      script: './.output/server/index.mjs'
+    }
+  ],
+  deploy: {
+    user: 'admin_web',
+    host: ['192.168.0.35'],
+    ref: 'origin/master',
+    repo: 'git@github.com:AlexeyNixel/frontend-ssr.git',
+    path: "/home/admin_web/infomania-front",
+    'post-deploy' : 'npm i && npm run build && pm2 startOrRestart ecosystem.config.cjs'
+  }
+}
