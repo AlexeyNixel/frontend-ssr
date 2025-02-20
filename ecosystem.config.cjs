c88eb18387549eb814756d3fefe8e30849bb26be
Pechenkin.A <pechenkin685@gmail.com>
2025-02-20 13:27:15 +0700

    deploy update

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -15,7 +15,7 @@ module.exports = {
     production: {
       user: 'admin_web',
       host: '192.168.0.35', // IP или домен сервера
-      ref: 'origin/main', // Ветка, на которую будем делать деплой
+      ref: 'origin/master', // Ветка, на которую будем делать деплой
       repo: 'https://github.com/AlexeyNixel/frontend-ssr.git', // URL репозитория
       path: '/home/admin_web/infomania-web/frontend', // Путь на сервере, куда будет клонирован репозиторий
       'post-deploy': 'npm install && npm run build && pm2 restart nuxt-app', // Действия после деплоя
