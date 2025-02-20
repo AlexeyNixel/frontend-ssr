175fcbd1f23a64498c8c56129e3798849d953df6
Pechenkin.A <pechenkin685@gmail.com>
2025-02-20 14:19:50 +0700

    deploy update

---
+++ ecosystem.config.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      name: 'Frontend',
+      port: '3000',
+      exec_mode: 'cluster',
+      instances: 'max',
+      script: './.output/server/index.mjs',
+    },
+  ],
+  deploy: {
+    production: {
+      user: 'admin_web',
+      host: ['192.168.0.35'],
+      ref: 'origin/master',
+      repo: 'https://github.com/AlexeyNixel/frontend-ssr.git',
+      path: '/home/admin_web/infomania-front',
+      'post-deploy':
+        'npm i && npm run build && pm2 restart ecosystem.config.cjs',
+    },
+  },
+};
