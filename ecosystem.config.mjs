324097d0dc452787b82abfbc58357a2dc4ab469d
Pechenkin.A <pechenkin685@gmail.com>
2025-02-20 14:16:56 +0700

    deplou update

--- ecosystem.config.mjs
+++ ecosystem.config.mjs
@@ -4,13 +4,6 @@ module.exports = {
       name: 'nuxt-app',
       script: './node_modules/nuxt/bin/nuxt.js',
       args: 'start',
-
-      env: {
-        NODE_ENV: 'development',
-      },
-
-      env_file: '.env', // Подключаем .env файл
-
       post_deploy: ['npm install', 'npm run build', 'pm2 restart nuxt-app'],
     },
   ],
