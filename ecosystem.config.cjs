c891d49062022f6748caf91b740578dde65f5df6
Pechenkin.A <pechenkin685@gmail.com>
2025-02-20 13:25:55 +0700

    deploy update

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,24 +1,27 @@
 module.exports = {
   apps: [
     {
-      name: 'frontend',
-      port: '3000',
-      exec_mode: 'cluster',
-      instances: 'max',
-      script: './.output/server/index.mjs',
+      name: 'nuxt-app',
+      script: './node_modules/nuxt/bin/nuxt.js',
+      args: 'start',
+      env: {
+        NODE_ENV: 'production',
+      },
+      post_deploy: ['npm install', 'npm run build', 'pm2 restart nuxt-app'],
     },
   ],
+
   deploy: {
     production: {
       user: 'admin_web',
-      host: ['192.168.0.35'],
-      ref: 'origin/master',
-      repo: 'https://github.com/AlexeyNixel/frontend-ssr.git',
-      post_deploy: [
-        'npm install', 
-        'npm run build', 
-        'pm2 restart nuxt-app', 
-      ],
+      host: '192.168.0.35', // IP или домен сервера
+      ref: 'origin/main', // Ветка, на которую будем делать деплой
+      repo: 'https://github.com/AlexeyNixel/frontend-ssr.git', // URL репозитория
+      path: '/home/admin_web/infomania-web/frontend', // Путь на сервере, куда будет клонирован репозиторий
+      'post-deploy': 'npm install && npm run build && pm2 restart nuxt-app', // Действия после деплоя
+      env: {
+        NODE_ENV: 'production',
+      },
     },
   },
 };
